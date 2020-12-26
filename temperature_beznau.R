bebc30e4fb825cbab3a9920f4646b4561dbec871
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2020-12-27 00:28:51 +0100

    Add files via upload
    added linear, trying to figure out the trigonometric part

--- temperature_beznau.R
+++ temperature_beznau.R
@@ -1,5 +1,6 @@
 # loading data
 library(ismev);
+library(evd);
 load(file="Project2020.Rdata")
 
 # allow to print entire dataset
@@ -15,4 +16,13 @@ temp_beznau=temp_beznau[-2:-1,]
 annual<-integer((nrow(temp_beznau)-11)/12+1)
 annual[1]<-max(temp_beznau$max[1:11])
 annual[2:length(annual)]<-sapply(split(temp_beznau$max[12:nrow(temp_beznau)], rep(1:(length(temp_beznau$max[12:nrow(temp_beznau)])/12), each=12)), max)
+
+# fitting GEV for stationary case
+monthly_fit = gev.fit(temp_beznau$max)
+annual_fit = gev.fit(annual)
+
+# linear model GEV for monthly maxima
+t=matrix(ncol=1,nrow=nrow(temp_beznau))
+t[,1]=seq(1,nrow(temp_beznau),1)
+gev.fit(temp_beznau$max,ydat=t,mul=1)
 
