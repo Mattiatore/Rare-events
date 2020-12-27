e5984c25d29d49019dc34b2685a437393266f7ac
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2020-12-27 13:33:26 +0100

    Add files via upload
    baseline working ( need to adpat for what they want)

--- temperature_beznau.R
+++ temperature_beznau.R
@@ -27,16 +27,16 @@ t[,1]=seq(1,nrow(temp_beznau),1)
 gev.fit(temp_beznau$max,ydat=t,mul=1)
 
 # model with trig functions?
-t=matrix(ncol=25,nrow=nrow(temp_beznau))
+t=matrix(ncol=9,nrow=nrow(temp_beznau))
 # choose t0 as 155
 t[,1] <- (seq(1,nrow(temp_beznau),1)-155)/(100+365.25)
 # sine 
-for (i in seq(1,12,1)) {
+for (i in seq(1,4,1)) {
   t[,i+1] <- sin(2*(i+1)*pi*(seq(1,nrow(temp_beznau),1)-155)/365.25)
 }
 # cos
-for (i in seq(1,12,1)) {
-  t[,i+13] <- sin(2*(i+13)*pi*(seq(1,nrow(temp_beznau),1)-155)/365.25)
+for (i in seq(1,4,1)) {
+  t[,i+5] <- sin(2*(i+5)*pi*(seq(1,nrow(temp_beznau),1)-155)/365.25)
 }
-gev.fit(temp_beznau$max,ydat=t,mul=25)
+gev.fit(temp_beznau$max,ydat=t,mul=c(1:9))
 
