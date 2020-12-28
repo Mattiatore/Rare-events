67942b48f29ba1df2d17596d3511ef297e12df29
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2020-12-28 17:36:18 +0100

    Add files via upload
    corrected version

--- temperature_beznau.R
+++ temperature_beznau.R
@@ -7,6 +7,7 @@ load(file="Project2020.Rdata")
 options(max.print=2000)
 
 # clear seasonality, maxima are during summer
+par(mfrow=c(1,1))
 plot(ts(temp_beznau$max))
 
 #remove 2013 since only two months?
@@ -19,14 +20,7 @@ annual[2:length(annual)]<-sapply(split(temp_beznau$max[12:nrow(temp_beznau)], re
 
 #fit for annual maxima
 annual_fit = gev.fit(annual)
-#r-year return level stationary case
-return_statio <- function(r){
-  return (annual_fit$mle[1]+annual_fit$mle[2]/annual_fit$mle[3]*(-1+(-log(1-1/r))**(-annual_fit$mle[3])))
-}
-print("Return levels for annual maxima based GEV and estimated standar error.")
-print(paste("100 year return level:",toString(return_statio(100)),"estiamted standard error",toString(return_statio_error(100)),sep=" "))
-print(paste("1000 year return level:",toString(return_statio(1000)),"estiamted standard error",toString(return_statio_error(1000)),sep=" "))
-print(paste("10000 year return level:",toString(return_statio(10000)),"estiamted standard error",toString(return_statio_error(10000)),sep=" "))
+
 # estimated errors for return levels
 return_statio_error <- function(r){
   y = -log(1-(1/r))
@@ -38,13 +32,22 @@ return_statio_error <- function(r){
   return (sqrt(del.transpose%*%annual_fit$cov%*%del))
 }
 
+#r-year return level stationary case
+return_statio <- function(r){
+  return (annual_fit$mle[1]+annual_fit$mle[2]/annual_fit$mle[3]*(-1+(-log(1-1/r))**(-annual_fit$mle[3])))
+}
+print("Return levels for annual maxima based GEV and estimated standar error.")
+print(paste("100 year return level:",toString(return_statio(100)),"estiamted standard error",toString(return_statio_error(100)),sep=" "))
+print(paste("1000 year return level:",toString(return_statio(1000)),"estiamted standard error",toString(return_statio_error(1000)),sep=" "))
+print(paste("10000 year return level:",toString(return_statio(10000)),"estiamted standard error",toString(return_statio_error(10000)),sep=" "))
+
 # fitting GEV for stationary case
 monthly_fit = gev.fit(temp_beznau$max)
 n <- nrow(temp_beznau)
 AIC_mon <- 2*monthly_fit$nllh+2*(2*K+3) +2*(2*K+3)*(2*K+4)/(n-2*K-4)
 
 # model with trig functions
-# K from 1 to 9
+# K from 1 to 6
 # assign t0 as in the description 
 t0 = as.Date("2000/01/01")
 diff <- seq(nrow(temp_beznau))
@@ -58,11 +61,11 @@ for (K in seq(6)){
   t[,1] <- diff/(100*365.25)
   # sine 
   for (i in seq(K)) {
-    t[,i+1] <- sin(2*(i+1)*pi*diff/365.25)
+    t[,i+1] <- sin(2*i*pi*diff/365.25)
   }
   # cos
   for (i in seq(K)) {
-    t[,i+K+1] <- cos(2*(i+1)*pi*diff/365.25)
+    t[,i+K+1] <- cos(2*i*pi*diff/365.25)
   }
   monthly_trig <- gev.fit(temp_beznau$max,ydat=t,mul=c(1:(2*K+1)))
   
@@ -78,17 +81,17 @@ AIC_monthly1 <- 2*monthly_trig$nllh+2*(2*K+3) +2*(2*K+3)*(2*K+4)/(n-2*K-4)
 # plot diagnostic for constant
 gev.diag(monthly_trig)
 
-# plot for best K=6
-K <- 6
+# plot for best K=1
+K <- 1
 t <- matrix(ncol=K*2+1,nrow=nrow(temp_beznau))
 t[,1] <- diff/(100*365.25)
 # sine 
 for (i in seq(K)) {
-  t[,i+1] <- sin(2*(i+1)*pi*diff/365.25)
+  t[,i+1] <- sin(2*i*pi*diff/365.25)
 }
 # cos
 for (i in seq(K)) {
-  t[,i+K+1] <- cos(2*(i+1)*pi*diff/365.25)
+  t[,i+K+1] <- cos(2*i*pi*diff/365.25)
 }
 monthly_trig <- gev.fit(temp_beznau$max,ydat=t,mul=c(1:(2*K+1)))
 
@@ -103,15 +106,15 @@ if((monthly_trig$mle[1]-1.96*monthly_trig$se[1])<0){
 
 # there is seasonal variation because some coefficients of the sin and cos are different from 0 at the 95% CI
 # test model without linear term u_1 in equation (1)
-K <- 6
+K <- 1
 t <- matrix(ncol=K*2,nrow=nrow(temp_beznau))
 # sine 
 for (i in seq(K)) {
-  t[,i] <- sin(2*(i+1)*pi*diff/365.25)
+  t[,i] <- sin(2*i*pi*diff/365.25)
 }
 # cos
 for (i in seq(K)) {
-  t[,i+K] <- cos(2*(i+1)*pi*diff/365.25)
+  t[,i+K] <- cos(2*i*pi*diff/365.25)
 }
 monthly_trig <- gev.fit(temp_beznau$max,ydat=t,mul=c(1:(2*K)))
 AIC_month <- 2*monthly_trig$nllh+2*(2*K) +2*(2*K)*(2*K+1)/(n-2*K-1)
@@ -119,3 +122,10 @@ AIC_month <- 2*monthly_trig$nllh+2*(2*K) +2*(2*K)*(2*K+1)/(n-2*K-1)
 # plot diagnostic for best model without U_1
 gev.diag(monthly_trig)
 
+# return levels for non stationary case
+return_non_statio <- function(r,t){
+  L=length(monthly_trig$mle)
+  T <- as.numeric((as.Date(t)-t0), unit="days")
+  somma <- monthly_trig$mle[2]*sin(2*pi*(T)/365.25) + monthly_trig$mle[3]*cos(2*pi*(T)/365.25)
+  return (somma + monthly_trig$mle[1]+monthly_trig$mle[L-1]/monthly_trig$mle[L]*(-1+(-log(1-1/r))**(-monthly_trig$mle[L])))
+}
